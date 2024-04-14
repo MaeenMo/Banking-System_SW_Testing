c4133d2246eee23dd101d8e01f8701eae310c74b
Omar Alaa Elnahass <21p0197@eng.asu.edu.eg>
2024-04-14 03:29:09 +0200

    removed added files

--- src/test/java/org/example/AllTests.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-import org.junit.platform.runner.JUnitPlatform;
-
-@SuppressWarnings("deprecation")
-@RunWith(JUnitPlatform.class)
-@SelectClasses({LoanTest.class, BankTest.class, TransactionTest.class, AccountTest.class})
-public class AllTests {}
